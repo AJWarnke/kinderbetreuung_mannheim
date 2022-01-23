c2b91cc6513eeef1284b54483b5c4a3e1c8cb24e
LAPTOP-E2DNT490\Arne <arne-warnke@gmail.com>
2022-01-23 08:26:20 +0100

    Updated graph

--- function_plot.R
+++ function_plot.R
@@ -5,22 +5,24 @@
 ###
 
 plot_voe = function(filename, title, subtitle, ts1, ts2, title_ts1, title_ts2, source) {
-  png(paste0(title, ".png"), width = 800, height = 400, units = "px")
-    plot(mannheim$date, rep(NA, nrow(mannheim)), ylim = c(0, 100), xaxt = "n", yaxt = "n", xlab = "", ylab = "")
-    mtext(title, line = 2.5, cex = 1.5)
-    mtext(subtitle, line = .85, cex = 1.15)
-    mtext("Versorgungsquote", side = 2, line = 2)
-    axis(1, year(mannheim[which(month(mannheim$date) == 7), "date"]), at = mannheim[which(month(mannheim$date) == 7), "date"], las = 1, cex = 2)
-    axis(2, paste0(seq(0, 100, 20), "%"), at = seq(0, 100, 20), las = 2)
+  png(paste0(title, ".png"), width = 1200, height = 600, units = "px")
+    par(mar = c(4.5, 5.3, 4.1, 4.1))
+    plot(mannheim$date, rep(NA, nrow(mannheim)), ylim = c(0, 100), xaxt = "n", yaxt = "n", xlab = "", ylab = "", yaxs = "i")
+    mtext(title, line = 2.5, cex = 2)
+    mtext(subtitle, line = .85, cex = 1.35)
+    mtext("Versorgungsquote", side = 2, line = 4, cex = 2)
+    axis(1, year(mannheim[which(month(mannheim$date) == 7), "date"]), at = mannheim[which(month(mannheim$date) == 7), "date"], las = 1, cex.axis = 1.5)
+    axis(2, paste0(seq(0, 100, 20), "%"), at = seq(0, 100, 20), las = 2, cex.axis = 1.35)
+    axis(4, paste0(seq(0, 100, 20), "%"), at = seq(0, 100, 20), las = 2, cex.axis = 1.35)
     abline(v = mannheim$date, col = "gray", lty = 3)
     abline(v = mannheim[which(month(mannheim$date) == 1), "date"], col = "darkred", lty = 2, lwd = 1)
     abline(h = seq(0, 100, 20), col = "gray", lty = 3)
     # Graphs
-    lines(mannheim$date, ts1, type = "b", pch = 16, lwd = 3, col = brewer.pal(3, "Set1")[3])
-    lines(mannheim$date, ts2, type = "b", pch = 16, lwd = 3, col = brewer.pal(3, "Set1")[1])
-    legend("bottom", c(title_ts1, title_ts2), pch = 16, col = brewer.pal(3, "Set1")[c(3,1)], cex = 1.25)
+    lines(mannheim$date, ts1, type = "b", pch = 15, lwd = 1.5, lty = 3, cex = 1.75, col = brewer.pal(3, "Set1")[3])
+    lines(mannheim$date, ts2, type = "b", pch = 15, lwd = 1.5, lty = 3, cex = 1.75, col = brewer.pal(3, "Set1")[1])
+    legend("bottom", c(title_ts1, title_ts2), pch = 16, col = brewer.pal(3, "Set1")[c(3,1)], cex = 1.5, ncol = 2)
     #
-    mtext(source, side = 1, adj = 1, line = -1, outer = TRUE)
+    mtext(source, side = 1, adj = 1, line = -1.1, outer = TRUE, cex = 1.25)
   dev.off()
 }
 
--- graphs.R
+++ graphs.R
@@ -11,20 +11,20 @@ mannheim = data[which(data$place == "Mannheim"), ]
 #
 plot_voe(filename = "Versorgungsquote_Krippe", 
          title = "Versorgungsquote Krippen Mannheim (U3)", 
-         subtitle = "Quelle: Stadt Mannheim",
+         subtitle = "Anteil Betreuungsplätze zu entsprechender Bevölkerungszahl",
          ts1 = lindenhof$Krippe.VQ,
          ts2 = mannheim$Krippe.VQ,
          title_ts1 = "Lindenhof",
          title_ts2 = "Mannheim",
-         source = "Eigene Darstellung; Quelle: Dezernat III, Stadt Mannheim"
+         source = "Eigene Darstellung (arne.warnke@gmail.com); Quelle: Dezernat III, Stadt Mannheim"
 )
 
 plot_voe(filename = "Versorgungsquote_Kindergarten", 
          title = "Versorgungsquote Kindergarten Mannheim (Ü3)", 
-         subtitle = "Quelle: Stadt Mannheim",
+         subtitle = "Anteil Betreuungsplätze zu entsprechender Bevölkerungszahl",
          ts2 = lindenhof$Kindergarten.VQ,
          ts1 = mannheim$Kindergarten.VQ,
          title_ts2 = "Lindenhof",
          title_ts1 = "Mannheim",
-         source = "Eigene Darstellung; Quelle: Dezernat III, Stadt Mannheim"
+         source = "Eigene Darstellung (arne.warnke@gmail.com); Quelle: Dezernat III, Stadt Mannheim"
 )
